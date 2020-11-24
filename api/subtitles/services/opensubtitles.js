8da9cdfab6c9c623909bdee7ba34afb039d6f03a
Max Arias <max@max-arias.com>
2020-11-23 22:36:46 -0300

    updated yts crawler, updated subs db

--- api/subtitles/services/opensubtitles.js
+++ api/subtitles/services/opensubtitles.js
@@ -1,24 +1,23 @@
 const OpenSubtitles = require('opensubtitles-api');
 
-const getSubs = async (imdbId) => {
+const getSubs = async imdbId => {
   const subs = new OpenSubtitles({
-    useragent: process.env.OPENSUBTITLES_USERAGENT
+    useragent: process.env.OPENSUBTITLES_USERAGENT,
   });
 
   const subs_found = await subs.search({ imdbid: imdbId });
 
-  const subtitles = Object.keys(subs_found).map((key) => (
-    {
-      data: subs_found[key],
-      language: subs_found[key].langcode,
-      imdbId,
-      provider: 'opensubtitles',
-    }
-  ));
+  const subtitles = Object.keys(subs_found).map(key => ({
+    data: subs_found[key],
+    language: subs_found[key].lang,
+    langcode: subs_found[key].langcode,
+    imdbId,
+    provider: 'opensubtitles',
+  }));
 
   return subtitles;
-}
+};
 
 module.exports = {
   getSubs,
-}
+};
--- crawlers/yts.js
+++ crawlers/yts.js
@@ -1,47 +1,50 @@
-const cheerio = require('cheerio')
-const fetch = require('node-fetch')
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 const { getCodeFromName } = require('../utils/CountryUtils');
 const BASE_URL = strapi.config.get('constants.YTS_URL', '/');
 
-const crawl = async (imdbId) => {
-  const body = await fetch(`${BASE_URL}/${imdbId}`).then((res) => res.text());
-  const $ = cheerio.load(body)
+const crawl = async imdbId => {
+  const body = await fetch(`${BASE_URL}/${imdbId}`).then(res => res.text());
+  const $ = cheerio.load(body);
 
-  const table = $('.table.other-subs')
+  const table = $('.table.other-subs');
 
   // No subs
   if (!table.length) {
-    return [{
-      imdbId,
-      language: 'None',
-      provider: 'yts',
-      data: null,
-    }];
+    return [];
   }
 
   const subs = [];
 
-  $('.table.other-subs').find('tbody tr').each((_, tr) => {
-    const languageLong = $(tr).find('.sub-lang').text();
-    const language = getCodeFromName(languageLong);
-    const filename = $(tr).find('td:nth-child(3)').text().replace(/\n|\t|(subtitle)/igm, '').trim();
-    const url = $(tr).find('.subtitle-download').attr('href');
+  $('.table.other-subs')
+    .find('tbody tr')
+    .each((_, tr) => {
+      const language = $(tr).find('.sub-lang').text();
+      const langcode = getCodeFromName(language);
+      const filename = $(tr)
+        .find('td:nth-child(3)')
+        .text()
+        .replace(/\n|\t|(subtitle)/gim, '')
+        .trim();
+      const url = $(tr).find('.subtitle-download').attr('href');
 
-    subs.push({
-      imdbId,
-      language,
-      provider: 'yts',
-      data: {
-        filename,
-        url,
-        lang: languageLong,
-        score: 0,
-      }});
-  });
+      subs.push({
+        imdbId,
+        language,
+        langcode,
+        provider: 'yts',
+        data: {
+          filename,
+          url,
+          lang: language,
+          score: 0,
+        },
+      });
+    });
 
   return subs;
-}
+};
 
 module.exports = {
   crawl,
-}
+};
